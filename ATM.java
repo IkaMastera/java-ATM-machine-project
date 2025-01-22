311c14885111f3dcbd5d2c3ef96b7fc6a172b4de
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-01-22 17:28:22 +0400

    update: ATM class

--- ATM.java
+++ ATM.java
@@ -1,2 +1,12 @@
-public class ATM {
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    System.out.println("Welcome to Iraklas ATM Machine");
+    System.out.println("[1]: Check Balance");
+    System.out.println("[2]: Withdraw");
+    System.out.println("[3]: Deposit");
+    System.out.println("[4]: Exit");
+    System.out.println("Enter Choice");
+    
 }
